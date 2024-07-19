ce1429cd50bb314d08297ba7eb1b98b5a7bdac65
Mu3ssam <94981148+3ssaamm@users.noreply.github.com>
2024-07-19 17:54:47 +0300

    Sensors Updates

--- Assets/scripts/Orientation Control.cs
+++ Assets/scripts/Orientation Control.cs
@@ -4,6 +4,17 @@ public class LanderOrientationController : MonoBehaviour
 {
     public float rotationSpeed = 100.0f; // Speed at which the lander rotates
 
+    private float pitchInputDuration = 0f;
+    private float yawInputDuration = 0f;
+    private float rollInputDuration = 0f;
+
+    private Vector3 previousRotation;
+
+    void Start()
+    {
+        previousRotation = transform.eulerAngles;
+    }
+
     void Update()
     {
         // Initialize rotation amounts for pitch, yaw, and roll
@@ -14,36 +25,64 @@ public class LanderOrientationController : MonoBehaviour
         // Check input and adjust the rotation amounts accordingly
         if (Input.GetKey(KeyCode.W))
         {
-            pitch = rotationSpeed * Time.deltaTime;
+            pitchInputDuration += Time.deltaTime;
+            pitch = rotationSpeed * pitchInputDuration;
         }
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
-            pitch = -rotationSpeed * Time.deltaTime;
+            pitchInputDuration += Time.deltaTime;
+            pitch = -rotationSpeed * pitchInputDuration;
         }
+        else
+        {
+            pitchInputDuration = 0;
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
-            yaw = -rotationSpeed * Time.deltaTime;
+            yawInputDuration += Time.deltaTime;
+            yaw = -rotationSpeed * yawInputDuration;
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            yaw = rotationSpeed * Time.deltaTime;
+            yawInputDuration += Time.deltaTime;
+            yaw = rotationSpeed * yawInputDuration;
         }
+        else
+        {
+            yawInputDuration = 0;
+        }
+
         if (Input.GetKey(KeyCode.Q))
         {
-            roll = rotationSpeed * Time.deltaTime;
+            rollInputDuration += Time.deltaTime;
+            roll = rotationSpeed * rollInputDuration;
         }
-        if (Input.GetKey(KeyCode.E))
+        else if (Input.GetKey(KeyCode.E))
         {
-            roll = -rotationSpeed * Time.deltaTime;
+            rollInputDuration += Time.deltaTime;
+            roll = -rotationSpeed * rollInputDuration;
+        }
+        else
+        {
+            rollInputDuration = 0;
         }
 
         // Apply rotations to the lander object
-        transform.Rotate(pitch, yaw, roll);
+        transform.Rotate(pitch * Time.deltaTime, yaw * Time.deltaTime, roll * Time.deltaTime);
 
-        // Log the angles moved
-        if (pitch != 0 || yaw != 0 || roll != 0)
+        // Log the change in rotation angles if all changes are greater than 5 degrees
+        Vector3 currentRotation = transform.eulerAngles;
+        Vector3 rotationChange = currentRotation - previousRotation;
+
+        if (Mathf.Abs(rotationChange.x) > 5 && Mathf.Abs(rotationChange.y) > 5 && Mathf.Abs(rotationChange.z) > 5)
         {
-            Debug.Log($"Pitch: {pitch}, Yaw: {yaw}, Roll: {roll}");
+            int pitchChange = Mathf.RoundToInt(rotationChange.x);
+            int yawChange = Mathf.RoundToInt(rotationChange.y);
+            int rollChange = Mathf.RoundToInt(rotationChange.z);
+
+            Debug.Log($"Rotation Change -> Pitch: {pitchChange}, Yaw: {yawChange}, Roll: {rollChange}");
+            previousRotation = currentRotation;
         }
     }
 }
--- Assets/scripts/Sensors.cs
+++ Assets/scripts/Sensors.cs
@@ -3,90 +3,116 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using System.Net.Sockets;
+using System.Text;
+
 public class Sensors : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //"Distance_Between_2_Centers_Of_Mars_And_Lander(Km)" is for distance between mars and the lander Which is measured by the altimeter sensor
+    private double Distance_Between_2_Centers_Of_Mars_And_Lander_Km;
+
+    public double Altitude_Km;
+
+    public float Landing_Site_Distance_Km;
+
+    //"i" is for iterations which is for calculating acceleration from calculating the average of the change rate of velocity vector in each frame  
+    private float i = 0;
+
+    //"Temperature_Of_Atmosphere(C)" is the temperature of atmosphere in altitude of the lander 
+    public double Temperature_Of_Atmosphere_C;
+
+    //"Temperature_Of_Lander(C)" is the temperature of the lander resulted from friction between lander and air molecules Which is measured by the temperature sensor
+    public double Temperature_Of_Lander_C;
+
+    //"Pressure_Of_Atmosphere(Pa)" is for the pressure of the atmosphere in altitude of the lander 
+    public double Pressure_Of_Atmosphere_Pa;
+
+    //"Pressure_On_Lander(Pa)" is for Pressure final which is the Pressure of the lander resulted from friction between lander and air molecules Which is measured by the pressure sensor
+    public double Pressure_On_Lander_Pa;
+
+    //Density(Kg/m^3) is for mars atmosphere density
+    public double Density_Kg_m3;
+    public double Drag_Force_N;
+    public double Drag_Coefficient;
+    public double Diameter_m;
+    public double Frontal_Area_m2;
+    public double Weight_Force_N;
+
+    //Heat_Gained(J) is the amount of heat gained or lost by the lander
+    public double Heat_Gained_J;
+
+    //S_Constant is a constant calculated from Mach number and specific heats of air 
+    public double S_Constant;
+
+    //M(Mach_Number) = vehicle flight Mach number
+    public double M_Mach_Number;
+
+    //Ratio_Of_The_Specific_Heats = ratio of the specific heats of air.
+    public double Ratio_Of_The_Specific_Heats;
+
+    //Accommodation_Coefficient = accommodation coefficient (taken as 1.0)
+    public double Accommodation_Coefficient;
+
+    // Specific_Heat is the specific heat
+    public double Specific_Heat;
+
+    //Lander_Mass(Kg) is mass of the lander
+    public double Lander_Mass_Kg;
+
+    //Adiabatic_Constant is the adiabatic constant.
+    public double Adiabatic_Constant;
+
+    //Gravitational_Acceleration(m/s^2) is gravitational acceleration on Mars
+    public double Gravitational_Acceleration_m_s2;
+
+    //"Timer(s)" is a timer to calculate the time from the moment we play the scene   
+    public double Timer_s;
+
+    //Angular_Velocity_Magnitude is for angularVelocity magnitude
+    public double Angular_Velocity_Magnitude;
+
+    public Vector3 currentAcceleration;
+    public Vector3 Magnetometer_Vector;
+    public Vector3 Position_Vector;
+    private Rigidbody rb;
+    private Vector3 lastVelocity;
+    private Vector3 Velocity_Vector;
+    public Vector3 lasrEulerAngle;
+    public Vector3 currentChangeRateOfEulerAngel;
+
     public GameObject Lander;
     public GameObject Mars;
     public GameObject North_Pole;
     public GameObject Landing_Site;
 
-    //"distance" is for distance between mars and the lander Which is measured by the altimeter sensor
-    public double distance;
-
-    public float Landing_Site_Distance;
-
-    //"i" is for iterations which is for calculating acceleration from calculating the average of the change rate of velocity vector in each frame  
-    public float i = 0;
-
-    //"T_int" is for temperature initial which is the temperature of atmosphere in altitude of the lander 
-    public double T_int;
-
-    //"T_final" is for Temperature final which is the temperature of the lander resulted from friction between lander and air molecules Which is measured by the temperature sensor
-    public double T_final;
-
-    //"Pressure" is for the pressure of the atmosphere in altitude of the lander 
-    public double Pressure;
-
-    //"Pressure_Final" is for Pressure final which is the Pressure of the lander resulted from friction between lander and air molecules Which is measured by the pressure sensor
-    public double Pressure_Final;
-
-    //Density is for mars atmosphere density
-    public double Density;
-    public double Drag_Force;
-    public double Drag_Coefficient;
-    public double Diameter;
-    public double Frontal_Area;
-    public double Weight_Force;
+    public string serverHost = "127.0.0.1";
+    public int serverPort = 25001;
 
 
-    //Q is the amount of heat gained or lost by the lander
-    public double Q;
 
-    //S is a constant claculated from  Mach number and specific heats of air 
-    public double S;
-
-    //M = vehicle flight Mach number
-    public double M;
-
-    //Y = ratio of the specific heats of air.
-    public double Y;
-
-    //a = accommodation coefficient (taken as 1.0)
-    public double a;
-
-    // c is the specific heat
-    public double c;
-
-    //m is mass of the lander
-    public double m;
-
-    //𝛾 is the adiabatic constant.
-    public double y;
-
-    //g is gravitational acceleration on Mars
-    public double g;
-
-    //"Time1" is a timer clculate the time from the moment we play the scene   
-    public double Time1;
-
-    //Gyro is for angularVelocity magnitude
-    public double Gyro;
-
-
-    public Vector3 Magnetometer_Vector;
-    public Vector3 Position_Vector;
-    Rigidbody rb;
-    public Vector3 Velocity_Vector;
-    public Vector3 Last_Velocity_Vector;
-    public Vector3 Acceleration_Vector;
-    public Vector3 Euler_Angles;
-    public Vector3 Angular_Velocity;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        Input.gyro.enabled = true;
+        lastVelocity = rb.velocity;
+        lasrEulerAngle = Lander.transform.eulerAngles;
+        //Input.gyro.enabled = true;
     }
+
+    void FixedUpdate()
+    {
+        //Accelometer Sensor code & Gyroscope Sensor Code calculated from change rate of velocity vector & change rate of Euler Angles
+        Vector3 currentVelocity = rb.velocity / 10; // Scale down by 10
+        Vector3 curentEulerAngle = Lander.transform.eulerAngles;
+        currentAcceleration = (currentVelocity - lastVelocity) / Time.fixedDeltaTime / 10; // Scale down by 10
+        currentChangeRateOfEulerAngel = (curentEulerAngle - lasrEulerAngle) / Time.fixedDeltaTime;
+        lastVelocity = currentVelocity;
+        lasrEulerAngle = curentEulerAngle;
+        Vector3 lander = Lander.transform.position;
+        Debug.DrawLine(lander, Mars.transform.position);
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -94,74 +120,84 @@ public class Sensors : MonoBehaviour
         Magnetometer_Vector = North_Pole.transform.position - Lander.transform.position;
 
         //Altimeter Sensor
-        distance = Vector3.Distance(Lander.transform.position, Mars.transform.position);
-        Landing_Site_Distance = Vector3.Distance(Lander.transform.position, Landing_Site.transform.position);
+        Distance_Between_2_Centers_Of_Mars_And_Lander_Km = Vector3.Distance(Lander.transform.position, Mars.transform.position) / 10; // Scale down by 10
+        Landing_Site_Distance_Km = Vector3.Distance(Lander.transform.position, Landing_Site.transform.position) / 10; // Scale down by 10
+
+        // Calculate Altitude_Km
+        Altitude_Km = Distance_Between_2_Centers_Of_Mars_And_Lander_Km - 3389.5;
 
         //State Vector of the lander relative to mars
-        Position_Vector = Lander.transform.position - Mars.transform.position;
+        Position_Vector = (Lander.transform.position - Mars.transform.position) / 10; // Scale down by 10
 
         //Timer Function 
-        Time1 = Time.time;
+        Timer_s = Time.time;
 
-        //Accelometer Sensor code claculated from average change rate of velocity vector
-        Velocity_Vector = rb.velocity;
-        i++;
-        if (i == 10)
+        //Nasa Function to calculate the temperature, pressure, and density of atmosphere when the altitude is known 
+        Velocity_Vector = rb.velocity / 10; // Scale down by 10
+        if (Distance_Between_2_Centers_Of_Mars_And_Lander_Km >= 700)
         {
-            Acceleration_Vector = (Velocity_Vector - Last_Velocity_Vector) / (10 * Time.deltaTime);
-            i = 0;
-            Last_Velocity_Vector = rb.velocity;
+            Temperature_Of_Atmosphere_C = -23.4 - 0.00222 * Distance_Between_2_Centers_Of_Mars_And_Lander_Km;
+            Pressure_Of_Atmosphere_Pa = 0.699 * Math.Exp(-0.00009 * Distance_Between_2_Centers_Of_Mars_And_Lander_Km);
+            Density_Kg_m3 = Pressure_Of_Atmosphere_Pa / (0.1921 * (Temperature_Of_Atmosphere_C + 273.1));
+        }
+        if (Distance_Between_2_Centers_Of_Mars_And_Lander_Km < 700)
+        {
+            Temperature_Of_Atmosphere_C = -31 - 0.000998 * Distance_Between_2_Centers_Of_Mars_And_Lander_Km;
+            Pressure_Of_Atmosphere_Pa = 0.699 * Math.Exp(-0.00009 * Distance_Between_2_Centers_Of_Mars_And_Lander_Km);
+            Density_Kg_m3 = Pressure_Of_Atmosphere_Pa / (0.1921 * (Temperature_Of_Atmosphere_C + 273.1));
         }
 
-        //Gyrscope Code
-        Euler_Angles = Lander.transform.eulerAngles;
-        Angular_Velocity = rb.angularVelocity;
-        Gyro = rb.angularVelocity.magnitude;
-
-        //Nasa Function to calculate the temperture , pressure and density of atmosphere when the altitude is known 
-        if (distance >= 7000)
-        {
-            T_int = -23.4 - 0.00222 * distance;
-            Pressure = 0.699 * Math.Exp(-0.00009 * distance);
-            Density = Pressure / (0.1921 * (T_int + 273.1));
-
-        }
-        if (distance < 7000)
-        {
-            T_int = -31 - 0.000998 * distance;
-            Pressure = 0.699 * Math.Exp(-0.00009 * distance);
-            Density = Pressure / (0.1921 * (T_int + 273.1));
-        }
-
-        //CALCULATIONS OF REENTRY-VEHICLE TEMPERATURE  Which is measured by the temperature sensor
+        //CALCULATIONS OF REENTRY-VEHICLE TEMPERATURE Which is measured by the temperature sensor
         //From Paper from INSTITUTE FOR DEFENSE ANALYSES 1 - 111 N.Bcauregutrd Strcct. Alexandria., Virginia 223! 1772
-        a = 1;
-        Y = 1.29;
-        M = Velocity_Vector.magnitude / 343;
-        S = M * Math.Sqrt(Y / 2);
-        m = 440;
-        c = 900;
-        T_final = (((a / 2) * Density * Math.Pow(Velocity_Vector.magnitude, 3) * (1 + (1 / (2 * Math.Sqrt(Math.PI) * S)))) / (m * c)) + T_int;
+        Accommodation_Coefficient = 1;
+        Ratio_Of_The_Specific_Heats = 1.29;
+        M_Mach_Number = Velocity_Vector.magnitude / 343;
+        S_Constant = M_Mach_Number * Math.Sqrt(Ratio_Of_The_Specific_Heats / 2);
+        Lander_Mass_Kg = 440;
+        Specific_Heat = 900;
+        Temperature_Of_Lander_C = (((Accommodation_Coefficient / 2) * Density_Kg_m3 * Math.Pow(Velocity_Vector.magnitude, 3) * (1 + (1 / (2 * Math.Sqrt(Math.PI) * S_Constant)))) / (Lander_Mass_Kg * Specific_Heat)) + Temperature_Of_Atmosphere_C;
 
-        // //CALCULATIONS OF REENTRY-VEHICLE Pressure  Which is measured by the pressure sensor
-        y = 4.3;
-        Pressure_Final = Math.Pow(Velocity_Vector.magnitude, 2) * Density / y;
+        // //CALCULATIONS OF REENTRY-VEHICLE Pressure Which is measured by the pressure sensor
+        Adiabatic_Constant = 4.3;
+        Pressure_On_Lander_Pa = Math.Pow(Velocity_Vector.magnitude, 2) * Density_Kg_m3 / Adiabatic_Constant;
 
-        //Calculations of draf force on the lander
-        g = 3.7;
+        //Calculations of draft force on the lander
+        Gravitational_Acceleration_m_s2 = 3.7;
         Drag_Coefficient = 0.24;
-        Diameter = 4.5;
-        Frontal_Area = Math.PI * Math.Pow(Diameter / 2, 2);
-        Weight_Force = m * g;
-        Drag_Force = 0.5 * Density * Drag_Coefficient * Math.Pow(Velocity_Vector.magnitude, 2) * Frontal_Area;
-  
+        Diameter_m = 4.5;
+        Frontal_Area_m2 = Math.PI * Math.Pow(Diameter_m / 2, 2);
+        Weight_Force_N = Lander_Mass_Kg * Gravitational_Acceleration_m_s2;
+        Drag_Force_N = 0.5 * Density_Kg_m3 * Drag_Coefficient * Math.Pow(Velocity_Vector.magnitude, 2) * Frontal_Area_m2;
 
-        
+        // Create a TCP client
+        TcpClient client = new TcpClient();
 
-    }
-    private void FixedUpdate()
-    {
-        Vector3 lander = Lander.transform.position;
-        Debug.DrawLine(lander, Mars.transform.position);
-    }
+        try
+        {
+            // Connect to the server
+            client.Connect(serverHost, serverPort);
+
+            // Get the Scale of the object
+
+            // Convert the Scale to a string
+            string dataToSend = '\n' + "currentAcceleration" + currentAcceleration.x + "," + currentAcceleration.y + "," + currentAcceleration.z + '\n' +
+                                "Magnetometer_Vector" + Magnetometer_Vector.x + "," + Magnetometer_Vector.y + "," + Magnetometer_Vector.z + '\n' +
+                                "currentChangeRateOfEulerAngel" + currentChangeRateOfEulerAngel.x + "," + currentChangeRateOfEulerAngel.y + "," + currentChangeRateOfEulerAngel.z;
+
+            // Send the data
+            NetworkStream stream = client.GetStream();
+            byte[] data = Encoding.UTF8.GetBytes(dataToSend);
+            stream.Write(data, 0, data.Length);
+            Debug.Log("Sent data to Python: " + dataToSend);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Error sending data to Python: " + e.Message);
+        }
+        finally
+        {
+            // Close the client
+            client.Close();
+        }
+    }
 }
